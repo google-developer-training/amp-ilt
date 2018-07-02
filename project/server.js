41d30dabdd077e71d1314fe31aa9bdd4874606e0
Nicolas Searle <nasearle@ucla.edu>
2018-07-02 15:46:51 -0700

    update(all): update project and basic solution
    This commit updates the project and basic solution folders to match the new code and format of the course. In particular, this commit:
    - removes the unused AMPstart CSS file, page.css
    - removes foo.html
    - updates main.css to match the styles in index.html
    - adds the thumbnail images, main.css, package.json, package-lock.json, and server.js to project and basic-solution

---
+++ project/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const multer = require('multer');
+
+const upload = multer();
+const app = express();
+
+// This serves static files from the specified directory
+app.use(express.static(__dirname));
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append('Content-Type', 'application/json');
+  next();
+});
+
+app.post('/submit-form', upload.array(), (req, res) => {
+  res.send(JSON.stringify(req.body));
+})
+
+const server = app.listen(8081, () => {
+
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
